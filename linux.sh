133d9da67225a143a0050652806c76c789a1b981
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2022-01-10 16:57:48 +0700

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,14 @@
+# linux Install
+
+# dont forget to sudo su
+
+```'''
+  pkg upgrade -y && pkg update -y
+  
+  apt update -y && apt full-upgrade -y
+  
+  pkg install python3 -y && sudo apt install python3-pip
+  
+  pip3 install requests
+  
+ '''```
