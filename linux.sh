0fc76d9dafec3ee3e071d792352a6ca8a48d721e
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2022-01-11 08:46:41 +0700

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -5,4 +5,4 @@
 pkg upgrade -y && pkg update -y  
 apt update -y && apt full-upgrade -y
 pkg install python3 -y && sudo apt install python3-pip
-pip3 install requests
+pip3 install -r requirements.txt
